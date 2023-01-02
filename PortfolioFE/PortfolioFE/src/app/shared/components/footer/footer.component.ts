d28197ff2f04916ae27f2058f786085d507ad38e
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2023-01-02 20:00:16 +0200

    [FE] Add icons to the footer

--- PortfolioFE/PortfolioFE/src/app/shared/components/footer/footer.component.ts
+++ PortfolioFE/PortfolioFE/src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  facebookUrl = 'https://www.facebook.com/mihainicolae.simedrea/';
+  instagramUrl = 'https://www.instagram.com/mihai_simedrea/';
+  linkedinUrl = 'https://www.linkedin.com/in/mihai-simedrea-80522421a/';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
