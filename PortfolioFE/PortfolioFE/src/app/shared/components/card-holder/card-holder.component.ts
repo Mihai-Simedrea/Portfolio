c28b97618fbe8fa4856efbfccc7d74ab4278461a
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2023-01-02 16:09:48 +0200

    [FE] Add UI for navbar

--- PortfolioFE/PortfolioFE/src/app/shared/components/card-holder/card-holder.component.ts
+++ PortfolioFE/PortfolioFE/src/app/shared/components/card-holder/card-holder.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-holder',
   templateUrl: './card-holder.component.html',
-  styleUrls: ['./card-holder.component.scss']
+  styleUrls: ['./card-holder.component.scss'],
 })
 export class CardHolderComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() animation: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
